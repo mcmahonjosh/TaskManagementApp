8812afe7c3177a12649c6b4b05d0f0fb86fdc296
Wizard10fun <wisard10fun@gmail.com>
2024-07-21 18:24:52 -0700

    debug = false for hosting the website

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@30r_!qmppm6l=dii8o%ba3xm!yvxpi^+7eogoxa_qpxm_)0%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
